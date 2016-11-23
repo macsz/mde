92c4d4cdaf33c096be79ddb71e85312277f9acbc
Maciej Szankin <maciej@szankin.pl>
2016-11-22 22:44:56 -0600

    updated the brew update script

--- brew/brew_update_lists.sh
+++ brew/brew_update_lists.sh
@@ -3,7 +3,16 @@
 # NOTE: this script does *not* update local packages
 # installed with brew. It is supposed to update lists
 # in this directory.
+#
+# Param (optional): directory that contains list of brew packages and casks
 
-brew list > brew.list
-brew cask list > brew.cask.list
-brew tap > brew.tap.list
+if [ -z ${1+x} ];
+then
+    DIR='.'
+else
+    DIR=$1
+fi
+
+brew list > $DIR/brew.list
+brew cask list > $DIR/brew.cask.list
+brew tap > $DIR/brew.tap.list
